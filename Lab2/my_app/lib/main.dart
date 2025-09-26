0972ce669a7ca7c44af20086576f5ea461903158
Ooozari <uzairasiff1227@gmail.com>
2025-09-26 11:32:05 +0500

    Lab 2: Add decrement button to counter app

--- Lab2/my_app/lib/main.dart
+++ Lab2/my_app/lib/main.dart
@@ -67,6 +67,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decCounter() {
+    if (_counter > 0) {
+      setState(() {
+        // This call to setState tells the Flutter framework that something has
+        // changed in this State, which causes it to rerun the build method below
+        // so that the display can reflect the updated values. If we changed
+        // _counter without calling setState(), then the build method would not be
+        // called again, and so nothing would appear to happen.
+        _counter--;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -112,11 +125,23 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10, height: 10),
+          FloatingActionButton(
+            onPressed: _decCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      ),
     );
   }
 }
