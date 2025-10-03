85646165f7f5dc07432a3832dab14e69bea5e341
Ooozari <uzairasiff1227@gmail.com>
2025-10-03 10:55:28 +0500

    Lab 3: Added Navbar to app

--- Lab3/lab3/lib/main.dart
+++ Lab3/lab3/lib/main.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyWidget());
+}
+
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AppBar(
+        title: const Text(
+          "Flutter Lab 03",
+          style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.black87,
+        foregroundColor: Colors.white,
+      ),
+    );
   }
 }
