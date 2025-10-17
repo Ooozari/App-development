ca2d1affe30011031aa4350e854f77da08913961
Ooozari <uzairasiff1227@gmail.com>
2025-10-17 12:31:03 +0500

    task6

--- Lab5/lab5/lib/main.dart
+++ Lab5/lab5/lib/main.dart
@@ -11,23 +11,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: ProductPage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ProductPage extends StatelessWidget {
+  const ProductPage({super.key});
 
-  // A helper function to show SnackBar
-  void showSnackBar(BuildContext context, String itemName) {
+  void addToCart(BuildContext context, String item) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text('Tapped on $itemName'),
-        duration: const Duration(seconds: 2),
+        content: Text("$item added to card"),
+        duration: const Duration(seconds: 3),
       ),
     );
   }
+
+  final List<Map<String, dynamic>> products = const [
+    {
+      'icon': Icons.smartphone,
+      'name': 'Smartphone',
+      'description': 'Powerful Android device with 8GB RAM',
+      'price': '\$799',
+    },
+    {
+      'icon': Icons.laptop,
+      'name': 'Laptop',
+      'description': 'High performance for work and gaming',
+      'price': '\$1,299',
+    },
+    {
+      'icon': Icons.watch,
+      'name': 'Smartwatch',
+      'description': 'Stay connected on the go',
+      'price': '\$299',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,129 +56,46 @@ class HomePage extends StatelessWidget {
         title: const Text(
           'Lab - 5',
           style: TextStyle(
-            color: Colors.black,
-            fontSize: 32,
+            fontSize: 26,
             fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
         ),
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
       ),
       body: ListView(
-        children: [
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.smartphone),
-            title: const Text('Smartphone'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Smartphone'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.laptop),
-            title: const Text('Laptop'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Laptop'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.tablet_mac),
-            title: const Text('Tablet'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Tablet'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.watch),
-            title: const Text('Smartwatch'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Smartwatch'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.headphones),
-            title: const Text('Headphones'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Headphones'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.desktop_windows),
-            title: const Text('Desktop Computer'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Desktop Computer'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.earbuds),
-            title: const Text('Wireless Earbuds'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Wireless Earbuds'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.sports_esports),
-            title: const Text('Gaming Console'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Gaming Console'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.tv),
-            title: const Text('Smart TV'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Smart TV'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.speaker),
-            title: const Text('Bluetooth Speaker'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Bluetooth Speaker'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.battery_charging_full),
-            title: const Text('Power Bank'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Power Bank'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.storage),
-            title: const Text('Portable Hard Drive'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Portable Hard Drive'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.flight),
-            title: const Text('Drone'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Drone'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.camera_alt),
-            title: const Text('Digital Camera'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Digital Camera'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.keyboard),
-            title: const Text('Keyboard'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Keyboard'),
-          ),
-          ListTile(
-            iconColor: Colors.blueAccent,
-            leading: const Icon(Icons.mouse),
-            title: const Text('Mouse'),
-            trailing: const Icon(Icons.bookmark),
-            onTap: () => showSnackBar(context, 'Mouse'),
-          ),
-        ],
+        padding: const EdgeInsets.all(12),
+        children: products.map((product) {
+          return Card(
+            elevation: 15,
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: ListTile(
+              leading: Icon(
+                product['icon'],
+                color: Colors.blueAccent,
+                size: 40,
+              ),
+              title: Text(
+                product['name'],
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              subtitle: Text(product['description']),
+              trailing: Text(
+                product['price'],
+                style: const TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              onTap: () => addToCart(context, product['name']),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
