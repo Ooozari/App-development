7fbb3b70bf53450da1e3080cf352fa50b231bfe3
Ooozari <uzairasiff1227@gmail.com>
2025-10-17 11:32:27 +0500

    task4

--- Lab5/lab5/lib/main.dart
+++ Lab5/lab5/lib/main.dart
@@ -23,7 +23,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Lab - 5',
           style: TextStyle(
             color: Colors.black,
@@ -39,7 +39,7 @@ class HomePage extends StatelessWidget {
           ListTile(
             iconColor: Colors.blueAccent,
             leading: Icon(Icons.smartphone),
-            title: Text('SmartPhone'),
+            title: Text('Smartphone'),
             trailing: Icon(Icons.bookmark),
           ),
           ListTile(
@@ -57,7 +57,7 @@ class HomePage extends StatelessWidget {
           ListTile(
             iconColor: Colors.blueAccent,
             leading: Icon(Icons.watch),
-            title: Text('SmartWatch'),
+            title: Text('Smartwatch'),
             trailing: Icon(Icons.bookmark),
           ),
           ListTile(
@@ -68,8 +68,20 @@ class HomePage extends StatelessWidget {
           ),
           ListTile(
             iconColor: Colors.blueAccent,
-            leading: Icon(Icons.speaker),
-            title: Text('Bluetooth Speaker'),
+            leading: Icon(Icons.desktop_windows),
+            title: Text('Desktop Computer'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.earbuds),
+            title: Text('Wireless Earbuds'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.sports_esports),
+            title: Text('Gaming Console'),
             trailing: Icon(Icons.bookmark),
           ),
           ListTile(
@@ -80,8 +92,44 @@ class HomePage extends StatelessWidget {
           ),
           ListTile(
             iconColor: Colors.blueAccent,
+            leading: Icon(Icons.speaker),
+            title: Text('Bluetooth Speaker'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.battery_charging_full),
+            title: Text('Power Bank'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.storage),
+            title: Text('Portable Hard Drive'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.flight),
+            title: Text('Drone'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
             leading: Icon(Icons.camera_alt),
-            title: Text('Camera'),
+            title: Text('Digital Camera'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.keyboard),
+            title: Text('Keyboard'),
+            trailing: Icon(Icons.bookmark),
+          ),
+          ListTile(
+            iconColor: Colors.blueAccent,
+            leading: Icon(Icons.mouse),
+            title: Text('Mouse'),
             trailing: Icon(Icons.bookmark),
           ),
         ],
