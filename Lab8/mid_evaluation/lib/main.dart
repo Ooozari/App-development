a50d02d7aac9b7106861faa3867d06fb0cd35a54
Ooozari <uzairasiff1227@gmail.com>
2025-11-10 17:36:56 +0500

    practice bulding custom themes

--- Lab8/mid_evaluation/lib/main.dart
+++ Lab8/mid_evaluation/lib/main.dart
@@ -4,50 +4,56 @@ void main() {
   runApp(const MyApp());
 }
 
+const String appName = "Themed App";
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: appName,
       theme: ThemeData(
-        sliderTheme: SliderThemeData(
-          activeTrackColor: Colors.red,
-          inactiveTrackColor: Colors.purple.shade200,
-          thumbColor: Colors.purple,
-          trackHeight: 5,
+        brightness: Brightness.light,
+        primaryColor: const Color.fromARGB(255, 2, 119, 189),
+        fontFamily: 'Georgia',
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headlineSmall: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
         ),
       ),
-      home: SliderExample(),
+      home: const HomePage(),
     );
   }
 }
 
-class SliderExample extends StatefulWidget {
-  @override
-  State<SliderExample> createState() => _SliderExampleState();
-}
-
-enum Status { passed, failed }
-
-class _SliderExampleState extends State<SliderExample> {
-  double _currentSliderValue = 10;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Slider Example")),
+      appBar: AppBar(title: Text(appName)),
       body: Center(
-        child: Slider(
-          value: _currentSliderValue,
-          min: 0,
-          max: 100,
-          divisions: 100,
-          onChanged: (value) {
-            setState(() {
-              _currentSliderValue = value;
-            });
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "DisplayLarge Text",
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "HeadlineSmall Text",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              "BodyMedium Example",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
         ),
       ),
     );
