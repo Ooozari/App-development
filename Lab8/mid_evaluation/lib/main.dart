9a97e1b9177820fd52355b806070d3a3c75cef2c
Ooozari <uzairasiff1227@gmail.com>
2025-11-10 14:43:28 +0500

    Practice Slider

--- Lab8/mid_evaluation/lib/main.dart
+++ Lab8/mid_evaluation/lib/main.dart
@@ -1,59 +1,42 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [GestureDetector].
+void main() {
+  runApp(const MyApp());
+}
 
-void main() => runApp(const GestureDetectorExampleApp());
-
-class GestureDetectorExampleApp extends StatelessWidget {
-  const GestureDetectorExampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: GestureDetectorExample());
+    return MaterialApp(home: SliderExample());
   }
 }
 
-class GestureDetectorExample extends StatefulWidget {
-  const GestureDetectorExample({super.key});
-
+class SliderExample extends StatefulWidget {
   @override
-  State<GestureDetectorExample> createState() => _GestureDetectorExampleState();
+  State<SliderExample> createState() => _SliderExampleState();
 }
 
-class _GestureDetectorExampleState extends State<GestureDetectorExample> {
-  bool _lightIsOn = false;
+class _SliderExampleState extends State<SliderExample> {
+  double _currentSliderValue = 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: FractionalOffset.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.lightbulb_outline,
-                color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
-                size: 60,
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  // Toggle light when tapped.
-                  _lightIsOn = !_lightIsOn;
-                });
-              },
-              child: Container(
-                color: Colors.yellow.shade600,
-                padding: const EdgeInsets.all(8),
-                // Change button text when light changes state.
-                child: Text(_lightIsOn ? 'TURN LIGHT OFF' : 'TURN LIGHT ON'),
-              ),
-            ),
-          ],
+      appBar: AppBar(title: const Text("Slider Example")),
+      body: Center(
+        child: Slider(
+          value: _currentSliderValue,
+          min: 0,
+          max: 100,
+          divisions: 100,
+          label: _currentSliderValue.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _currentSliderValue = value;
+            });
+          },
         ),
       ),
     );
