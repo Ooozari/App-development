0e157daf189c61601f1ac5d5560908405740fb40
Ooozari <uzairasiff1227@gmail.com>
2025-11-10 19:55:22 +0500

    Practise launchURL

--- Lab8/mid_evaluation/lib/main.dart
+++ Lab8/mid_evaluation/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,49 +10,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const BoxDecorationExample());
+    return MaterialApp(home: DemoPage());
   }
 }
 
-class BoxDecorationExample extends StatelessWidget {
-  const BoxDecorationExample({super.key});
+class DemoPage extends StatelessWidget {
+  launchURL() {
+    Uri.parse('https://flutter.dev');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("BoxDecoration Example")),
       body: Center(
-        child: Container(
-          width: 300,
-          height: 300,
-          decoration: BoxDecoration(
-            color: const Color(0xff7c94b6), // Background color
-            image: const DecorationImage(
-              image: NetworkImage(
-                'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
-              ),
-              fit: BoxFit.cover, // Cover the entire box
-            ),
-            border: Border.all(color: Colors.black, width: 1),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(2),
-              bottomLeft: Radius.circular(8),
-              bottomRight: Radius.circular(20),
-            ),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.green,
-                blurRadius: 10,
-                offset: Offset(5, 5),
-              ),
-              BoxShadow(
-                color: Colors.red,
-                blurRadius: 10,
-                offset: Offset(2, 2),
-              ),
-            ],
-          ),
+        child: ElevatedButton(
+          onPressed: launchURL,
+          child: Text('Show Flutter homepage'),
         ),
       ),
     );
